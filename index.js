7cb13c8cc41a13e51a02ae9c27c6cbd4be5e371f
James Maweu <jambonyote@protonmail.com>
2025-09-25 10:06:09 +0300

    Merge changes : sending randoms facts to all subscribed users

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ console.log('[DEBUG] AT_USERNAME =', process.env.AT_USERNAME);
 console.log('[DEBUG] AT_API_KEY starts with =', process.env.AT_API_KEY?.slice(0,10));
 const express = require('express');
 const bodyParser = require('body-parser');
+const { broadcastFact } = require('./services/facts');
 
 const smsRoutes = require('./routes/sms');
 const adminRoutes = require('./routes/admin');
 const airtimeRoutes = require('./routes/airtime');
+
+// every minute (60,000 ms) send a fact regardless of user activity
+setInterval(() => {
+  console.log('[DEBUG] Broadcasting fact to users');
+  broadcastFact();
+}, 60 * 1000);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
--- services/registration.js
+++ services/registration.js
@@ -27,7 +27,7 @@ function startRegistration(phone) {
     alreadyRegistered: false,
     message:
       'Welcome! To register, reply with: NAME Grade Subject(s).\n' +
-      'Example: Alice Grade6 Mathematics,English\n\n' +
+      'Example: Alice Grade6 Math,English\n\n' +
       'Available subjects: ' + AVAILABLE_SUBJECTS.join(', ')
   };
 }
